e5f6d666194ddcaf48025bcfae001af2c011b5a7
xrr2016 <13286904669@163.com>
2016-12-30 23:29:37 +0800

    :hankey: edit

--- src/api.js
+++ src/api.js
@@ -3,17 +3,32 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://news-at.zhihu.com/api/4'
 
 export default {
-  getLatest(){
-    const url = '/news/latest'
-    axios.get(url)
-        .then( res =>{
-          console.log(res.data)
-        })
-        .catch(err => {
-          console.log(err.message)
-        })
-  },
-  getSingleNews(newsId){
-        
-  }
+    getLatest() {
+        const url = '/news/latest'
+        axios.get(url)
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+    },
+    getSingleNews(newsId) {
+        const url = `/news/${newsId}`
+        axios.get(url)
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+    }
+}
+
+function isOrigin(data) {
+    if(data.type === 0){
+      return true
+    }else{
+      return false
+    }
 }
