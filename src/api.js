aa72f9618839182dd45f3212b7333be86101925e
xrr2016 <13286904669@163.com>
2016-12-30 22:16:06 +0800

    :fire: edit

--- src/api.js
+++ src/api.js
@@ -1,6 +1,19 @@
 import axios from 'axios'
 
+axios.defaults.baseURL = 'http://news-at.zhihu.com/api/4'
 
 export default {
-
+  getLatest(){
+    const url = '/news/latest'
+    axios.get(url)
+        .then( res =>{
+          console.log(res.data)
+        })
+        .catch(err => {
+          console.log(err.message)
+        })
+  },
+  getSingleNews(newsId){
+        
+  }
 }
