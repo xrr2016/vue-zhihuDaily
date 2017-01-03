97c436be73e2fb19693889f31beda0cc47d6b995
xrr2016 <13286904669@163.com>
2017-01-03 23:18:08 +0800

    :hankey: edit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,41 @@
 <template>
   <div id="app">
+    <md-icon>Home</md-icon>
     知乎日报
+    <div class="phone-viewport">
+  <md-toolbar>
+    <md-button class="md-icon-button" @click="toggleLeftSidenav">
+      <md-icon>menu</md-icon>
+    </md-button>
+
+    <h2 class="md-title">My App</h2>
+  </md-toolbar>
+
+  <div>
+    <md-button class="md-raised md-accent" @click="toggleRightSidenav">Toggle right</md-button>
+    <p>Open console to see the events</p>
+  </div>
+
+  <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+    <md-toolbar class="md-large">
+      <div class="md-toolbar-container">
+        <h3 class="md-title">Sidenav content</h3>
+      </div>
+    </md-toolbar>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi cupiditate esse necessitatibus beatae nobis, deserunt ut est fugit, tempora deleniti, eligendi commodi doloribus. Nemo, assumenda possimus, impedit inventore perferendis iusto!</p>
+  </md-sidenav>
+
+  <md-sidenav class="md-right" ref="rightSidenav" @open="open('Right')" @close="close('Right')">
+    <md-toolbar>
+      <div class="md-toolbar-container">
+        <h3 class="md-title">Sidenav content</h3>
+      </div>
+    </md-toolbar>
+
+    <md-button class="md-raised md-accent" @click="closeRightSidenav">Close</md-button>
+  </md-sidenav>
+</div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 
+import VueMaterial from 'vue-material'
+
+Vue.use(VueMaterial)
 
 new Vue({
-  el: '#app',
   render: h => h(App)
-})
+}).$mount('#app')
