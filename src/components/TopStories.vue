050a977e6a7bb0f8f749a294345b6898460b9a12
xrr2016 <13286904669@163.com>
2017-01-09 01:11:33 +0800

    :hankey: ...

--- src/components/TopStories.vue
+++ src/components/TopStories.vue
@@ -1,22 +1,35 @@
 <template lang="html">
   <div class="topStories">
-    {{ topStories }}
+    <ul>
+      <li v-for="story of this.topStories"> {{ story.title }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import API from '../api.js'
+// import API from '../api.js'
 export default {
-  data(){
-    return{
-      topStories : API.getLatest()
-    }
-  },
-  computed:{
-  }
+    created() {
+    },
+    data() {
+        return {
+          topStories : this.$get('/news/latest')
+                           .then(res => {
+                            const topStories = res.data.top_stories
+                            return topStories
+                          })
+                          .catch((err) => {
+                            console.log(err.message)
+                          })
+                }
+    },
+    computed: {
+
+    },
+    methods: {}
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .topStories{}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
+const JsonBird = 'https://bird.ioliu.cn/v1/?url='
+axios.defaults.baseURL = `${JsonBird}http://news-at.zhihu.com/api/4`
+
 Vue.prototype.$get = axios.get
 
 new Vue({
-  created:(){
+  created(){
   },
   render: h => h(App)
 }).$mount('#app')
